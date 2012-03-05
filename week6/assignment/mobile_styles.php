3f3ab4b21c22313ca7ee72fdeac659ff6fe45d52
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2012-03-05 10:04:54 +0100

    del mobile_styles.php & screen_styles.php
    We use htaccess so we do note need anymore these files

--- week6/assignment/mobile_styles.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type: text/css; charset=utf-8");
-header("Cache-Control: max-age=36000");
-?>
-body {font-family:sans-serif; color:#ccf; background-color:black}
-a {color:red}
-
--- week6/assignment/screen_styles.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: text/css; charset=utf-8");
-header("Cache-Control: max-age=36000");
-?>
-
-body {font-family:sans-serif; color:black; background-color:#ccf}
