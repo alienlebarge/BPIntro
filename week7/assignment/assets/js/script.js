b23244dd52675dc25a3ce3d25a273dfc994b2caa
alienlebarge <cedric.aellen@alienlebarge.ch>
2012-03-14 15:04:36 +0100

    button coloration

--- week7/assignment/assets/js/script.js
+++ week7/assignment/assets/js/script.js
@@ -12,19 +12,20 @@ function getAlfredNumber() {
         // if yes
         alfredNumber = JSON.parse(localStorage.getItem('alfredNumber'));
 
-        alert('Alfred allready choose ' + alfredNumber);
+        //alert('Alfred allready choose ' + alfredNumber);
     } else {
         // if not
-        alert("Alfred should choose a number");
+        //alert("Alfred should choose a number");
 
         alfredNumber = Math.floor(Math.random() * 10);
         localStorage.setItem('alfredNumber', JSON.stringify(alfredNumber));
 
-        alert("Alfred choose " + alfredNumber);
+        //alert("Alfred choose " + alfredNumber);
     }
 
     // write result
     document.getElementById('alfredNumber').innerText = alfredNumber;
+
     return alfredNumber;
 }
 
@@ -37,16 +38,16 @@ function getScore() {
 
         // if yes, we get the stored score
         score = localStorage.getItem('score');
-        alert('your score is ' + score);
 
     } else {
 
         // if note we set it to 0
         score = 0;
         localStorage.setItem('score', score);
-        alert('your score is ' + score);
 
     }
+
+    //alert('your score is ' + score);
 
     // write result
     document.getElementById('score').innerText = score;
@@ -58,34 +59,52 @@ function attempt(number) {
 
     var score = JSON.parse(localStorage.getItem('score')) + 1;
     localStorage.setItem('score', JSON.stringify(score));
+    localStorage.setItem(number, 1);
 
     // is choosen number Alfred's number ?
     if (number == getAlfredNumber()) {
         // yes !
         alert('You win after ' + getScore() + ' attempt(s)');
-
+        reset();
     }
 
     // print score
     getScore();
+    getButtonColor ();
+}
 
+function getButtonColor () {
+    // check wich number are allready choosen.
+    for (i=0; i<=9; i++) {
+        if (localStorage.getItem(i) == 1) {
+            document.getElementById('choose-'+i).className='allreadyChoosen';
+        } else {
+            document.getElementById('choose-'+i).className='';
+        }
+    }
 }
 
 function reset() {
+    // set score to 0
     score = 0;
     localStorage.setItem('score', score);
+    // remove alfredNumber
     localStorage.removeItem('alfredNumber');
-    alert('Storage is cleared');
+    // set all choosen number to 0
+    for (i=0; i<=9; i++) {
+        localStorage.setItem(i, 0);
+    }
     init();
 }
 
 function init() {
-    alert("Game initialisation !!!");
+    //alert("Game initialisation !!!");
 
     if (isLocalStorageSupported()) {
 
         //localstorage is supported
 
+        // event listener
         document.getElementById('choose-0').onclick = function () {
             attempt(0);
         };
@@ -122,6 +141,8 @@ function init() {
 
         getAlfredNumber();
         getScore();
+        getButtonColor ();
+
 
     } else {
         //localstorage is not supported
